3dc0fafd06857ae2a9993e36552db67ecf2f28b8
macscig <maciej.scigala@oke.pl>
2023-06-20 16:53:31 +0200

    Add even more cart tests

--- tests/cart02.spec.ts
+++ tests/cart02.spec.ts
@@ -1,33 +1,35 @@
 import { test, expect, devices } from '@playwright/test';
 
 test('Buy 10 T-shirts', async ({ page }) => {
-  page.setViewportSize(devices['iPhone X'].viewport);
+  //page.setViewportSize(devices['iPhone X'].viewport);
   await page.goto('https://shop.teamgullit.com/');
   await page.getByRole('link', { name: 'Shop now' }).click();
   await page.getByText('L', { exact: true }).click();
   await page.getByRole('button', { name: 'Add to Cart' }).click();
   await page.getByLabel('Special instructions for seller').click();
   await page.getByLabel('Special instructions for seller').fill('Hello');
+
+  function increaseNumberOfItems(number: number) {
+    
+  }
   await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
   await expect(page.locator('input.ajaxcart__qty-num')).toHaveValue('2')
-  await page.getByLabel('Special instructions for seller').click();
   await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
   await expect(page.locator('input.ajaxcart__qty-num')).toHaveValue('3')
-  await page.getByLabel('Special instructions for seller').click();
   await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
   await expect(page.locator('input.ajaxcart__qty-num')).toHaveValue('4')
-  await page.getByLabel('Special instructions for seller').click();
-  await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
-  await page.getByLabel('Special instructions for seller').click();
-  await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
-  await page.getByLabel('Special instructions for seller').click();
-  await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
-  await page.getByLabel('Special instructions for seller').click();
-  await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
-  await page.getByLabel('Special instructions for seller').click();
-  await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
-  await page.getByLabel('Special instructions for seller').click();
-  await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
+
+  // await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
+  // await page.getByLabel('Special instructions for seller').click();
+  // await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
+  // await page.getByLabel('Special instructions for seller').click();
+  // await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
+  // await page.getByLabel('Special instructions for seller').click();
+  // await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
+  // await page.getByLabel('Special instructions for seller').click();
+  // await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
+  // await page.getByLabel('Special instructions for seller').click();
+  // await page.getByRole('button', { name: 'Increase item quantity by one' }).click();
   await page.getByRole('button', { name: 'Check Out' }).click();
   await page.getByPlaceholder('Email').click();
 });
